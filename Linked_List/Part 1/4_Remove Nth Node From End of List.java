1961509cf6d0d63656c53508ad7ef812d3af92a9
Moin Ali <moinali7863@gmail.com>
2023-06-18 00:18:32 +0530

    Update 4_Remove Nth Node From End of List.java

--- Linked_List/Part 1/4_Remove Nth Node From End of List.java
+++ Linked_List/Part 1/4_Remove Nth Node From End of List.java
@@ -1,1 +1,19 @@
-...
+class Solution {
+    int n;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next==null)return null;
+        this.n=n;
+        ListNode preHead=new ListNode(0);
+        preHead.next=head;
+        removeNode(preHead);
+        return preHead.next;
+    }
+    public void removeNode(ListNode head)
+    {
+        if(head.next==null)
+        {--n;return;}
+        removeNode(head.next);
+        if(n==0) head.next=head.next.next;
+        --n;
+    }
+}
