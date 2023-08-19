9038424d09918a7c3aff016d6635470bba8aedbf
Moin Ali <moinali7863@gmail.com>
2023-08-19 22:34:13 +0530

    Update 2_DFS_of_graph.java

--- Graphs/2_DFS_of_graph.java
+++ Graphs/2_DFS_of_graph.java
@@ -1,5 +1,5 @@
 class Solution {
-    // Function to return a list containing the DFS traversal of the graph.
+    
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean[] vis= new boolean[V];
         ArrayList<Integer> ans= new ArrayList<>();
