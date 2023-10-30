151aab91e5d54b50c50323b29341870eeab84f29
Moin Ali <moinali7863@gmail.com>
2023-10-30 20:25:00 +0530

    30-10-23

---
+++ Binary Search Extra/A2Z/koko eating bananas.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+       int s=1,e=0;
+       for(int i:piles) e=Math.max(e,i); 
+
+       while(s<e)
+       {
+           int mid=s+(e-s)/2;
+           int hrs=0;
+           for(int i:piles)
+           {
+               int cur=(int)Math.ceil((double)i/mid);
+               hrs+=cur;
+           }
+           if(hrs>h)
+               s=mid+1;
+           else
+               e=mid;  
+       }
+       return s;
+    }
+}
--- Binary Search Extra/Find_peak_element.java
+++ Binary Search Extra/Find_peak_element.java
@@ -1,4 +1,19 @@
 // https://leetcode.com/problems/find-peak-element/description/
+
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int s=0,e=nums.length-1;
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]<nums[mid+1])s=mid+1;
+            else e=mid;        }
+        return s;
+    }
+}
+
+
 class Solution {
     public int findPeakElement(int[] nums) {
         if(nums.length==1)return 0;
