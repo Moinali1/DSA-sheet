f489211debeb68d3e2bf267835788dcbbb0fcdfa
Moin Ali <moinali7863@gmail.com>
2023-11-17 22:07:16 +0530

    17-11-23

---
+++ Binary_trees/26_Diameter of a Binary Tree.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    
+    class Pair{
+        int d,h;
+        Pair(int d,int h)
+        {
+            this.d=d;this.h=h;
+        }
+    }
+    int diameter(Node root) {
+        if(root==null)return 0;
+        return getDia(root,1).d;
+    }
+    
+    Pair getDia(Node node,int height)
+    {
+        if(node==null)return new Pair(0,0);
+        
+        Pair left= getDia(node.left,height+1);
+        Pair right= getDia(node.right,height+1);
+        
+        int curH=Math.max(left.h,right.h)+1;
+        int curD=Math.max(Math.max(left.d,right.d),left.h+right.h+1);
+        
+        return new Pair(curD,curH);
+    }
+}
