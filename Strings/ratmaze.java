85037842d67b3e69f1cf4d8f19f4c7ca37f640de
Moin Ali <moinali7863@gmail.com>
2024-03-03 22:41:54 +0530

    27-02-2024

---
+++ Strings/ratmaze.java
@@ -0,0 +1,32 @@
+
+// User function Template for Java
+
+// m is the given matrix and n is the order of matrix
+class Solution {
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        ArrayList<String> ans= new ArrayList<>();
+        getPath(0,0,"",m,ans,n);
+        return ans;
+    }
+    public static void getPath(int r,int c, String s,int[][] m,ArrayList<String> ans,int n)
+    {
+        if(r<0 || r>=n ||c<0 ||c>=n)return;
+        if(m[r][c]==0)return;
+        if(r==n-1 && c==n-1)
+        {
+            ans.add(s);
+            return;
+        }
+            
+            m[r][c]=0;
+            getPath(r+1,c,s+"D",m,ans,n);    
+       
+            getPath(r,c+1,s+"R",m,ans,n);    
+    
+            getPath(r-1,c,s+"U",m,ans,n);    
+    
+            getPath(r,c-1,s+"L",m,ans,n);    
+            m[r][c]=1;
+        
+    }
+}
